57c8b7f2800db8b99bcc17c98cb14a671ab40363
Supratim Das <86043072+SupratimDas20@users.noreply.github.com>
2023-07-03 21:01:05 +0530

    Create 3rd July, Maximum Index.cpp

---
+++ 3rd July, Maximum Index.cpp
@@ -0,0 +1,31 @@
+
+
+class Solution {
+public:
+    int maxIndexDiff(int arr[], int n) {
+        int maxDiff = 0;
+        vector<int> maxJ(n);
+        
+        for (int i = 0; i < n; i++)
+            maxJ[i] = i;
+            
+        for (int i = 1; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] <= arr[i])
+                    maxJ[i] = j;
+                
+                if (maxJ[j] == j) {
+                    if (arr[j] > arr[i])
+                        break;
+                }
+                else if (arr[maxJ[j]] <= arr[i])
+                    j = maxJ[j] + 1;
+            }
+            
+            maxDiff = max(maxDiff, i - maxJ[i]);
+        }
+        
+        return maxDiff;
+    }
+};
+
